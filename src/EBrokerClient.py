c797c7cacbe8117d71cdaa3b08cd8db9d44864fc
Vojtech Juranek <vojtech.juranek@gmail.com>
2014-09-28 22:15:03 +0200

    One underscore in private attributes

--- src/EBrokerClient.py
+++ src/EBrokerClient.py
@@ -11,7 +11,7 @@ class EBrokerClient:
     __metaclass__ = abc.ABCMeta
 
     def __init__(self, executable, login, password):
-        self.__browser = self._create_browser(executable)
+        self._browser = self._create_browser(executable)
         self.login(login, password)
         return
 
@@ -20,22 +20,22 @@ class EBrokerClient:
         return
 
     def login(self, login, password):
-            self.__browser.get("https://www.fio.cz/e-broker/e-broker.cgi")
-            assert "e-Broker: Login" in self.__browser.title
-            self.__browser.find_element_by_name("LOGIN_USERNAME").send_keys(login)
-            self.__browser.find_element_by_name("LOGIN_PASSWORD").send_keys(password)
-            self.__browser.find_element_by_name("SUBMIT").click()
-            assert "Portfolio/Stav" in self.__browser.title
+            self._browser.get("https://www.fio.cz/e-broker/e-broker.cgi")
+            assert "e-Broker: Login" in self._browser.title
+            self._browser.find_element_by_name("LOGIN_USERNAME").send_keys(login)
+            self._browser.find_element_by_name("LOGIN_PASSWORD").send_keys(password)
+            self._browser.find_element_by_name("SUBMIT").click()
+            assert "Portfolio/Stav" in self._browser.title
 
     def close(self):
-        self.__browser.close()
+        self._browser.close()
 
     def get_broker_requests(self):
-        self.__browser.get("https://www.fio.cz/e-broker/e-pokyny.cgi")
-        assert "Pokyny" in self.__browser.title
+        self._browser.get("https://www.fio.cz/e-broker/e-pokyny.cgi")
+        assert "Pokyny" in self._browser.title
 
         requests = []
-        html = self.__browser.execute_script("return document.getElementById('pokyny_full_table').innerHTML")
+        html = self._browser.execute_script("return document.getElementById('pokyny_full_table').innerHTML")
         soup = BeautifulSoup(html)
         for tr in soup.find_all('tr'):
             tds = tr.find_all('td')
