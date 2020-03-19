5a29bdb5a3ad4cb247e7e0c820d9655058ebbd45
Vojtech Juranek <vojtech.juranek@gmail.com>
2020-03-19 22:12:46 +0100

    Get rid off BeautifulSoup warning

--- client/EBrokerClient.py
+++ client/EBrokerClient.py
@@ -44,7 +44,7 @@ class EBrokerClient:
         requests = []
         #html = self._browser.execute_script("return document.getElementById('pokyny_full_table').innerHTML")
         html = self._browser.find_element_by_id("pokyny_full_table").get_attribute("innerHTML")
-        soup = BeautifulSoup(html)
+        soup = BeautifulSoup(html, features="lxml")
         trs = soup.find_all("tr")
         for i in range(2, len(trs)): 
             tds = trs[i].find_all("td")
